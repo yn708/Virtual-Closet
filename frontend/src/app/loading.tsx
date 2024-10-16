a8925e08fccebd099ffc8cccd6cd2a631a64b162
yn <yamahon708@gmail.com>
2024-10-16 15:26:47 +0900

    first setup complete

---
+++ frontend/src/app/loading.tsx
@@ -0,0 +1,7 @@
+import LoadingElements from '@/components/elements/loading/LoadingElements';
+
+const Loading = () => {
+  return <LoadingElements fullScreen={true} />;
+};
+
+export default Loading;
---
+++ frontend/src/utils/fonts.ts
@@ -0,0 +1,6 @@
+import { Noto_Sans_JP } from 'next/font/google';
+
+export const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+  variable: '--font-noto-sans-jp',
+});
